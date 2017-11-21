c099be4985a1139f6edfde331a0dcc87ff55503b
meghanagabhushan <nagabhushan.megha@gmail.com>
2017-11-20 21:51:54 -0600

    updated code
    updated code

--- source/EmergencyVehicle/src/Request.java
+++ source/EmergencyVehicle/src/Request.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,72 +6,51 @@ import java.util.List;
      * Created by Megha Nagabhushan on 11/5/2017.
      */
     public class Request {
-        public static void main(String[] args) throws IOException{
+        public static void main(String[] args) {
 
-            new Thread(() -> startRequest()).start();
+            new Thread(() -> {
+                try {
+                    startRequest();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
             new Thread(() -> completeRequest()).start();
-            List<EmergencyVehicle> vehicleObjList = new ArrayList<>();
-            File distanceFile = new File("data/EmergencyVehicle.csv");
-            BufferedReader br = new BufferedReader(new FileReader(distanceFile));
-            String line="";
-            while ((line = br.readLine()) != null) {
-                String[] attributes = line.split(",");
-                EmergencyVehicle emergencyVehicles = createEmergencyVehicle(attributes);
-                vehicleObjList.add(emergencyVehicles);
-            }
-            br.close();
-
-            List<RequestVehicle> requestObjList = new ArrayList<>();
-            File reqFile = new File("data/TableRequest.csv");
-            BufferedReader breader = new BufferedReader(new FileReader(reqFile));
-            String line1="";
-            while ((line1 = breader.readLine()) != null) {
-                String[] lines = line1.split(",");
-                RequestVehicle requestVehicles = createRequestVehicle(lines);
-                requestObjList.add(requestVehicles);
-            }
-            breader.close();
-
-           for(RequestVehicle r : requestObjList){
-                System.out.println("VehicleType: " + r.vehicleType);
-                System.out.println("ZipCode: " + r.zipCode);
-               System.out.println("Requested Vehicles: " + r.number);
-                requestVehicle(r.vehicleType,r.zipCode, r.number,vehicleObjList);
-            }
-
 
 
         }
-        public static void requestVehicle(int requestType, int zipcode, int number,List<EmergencyVehicle> vehicleObj) throws IOException {
 
+
+
+
+        public static void requestVehicle(int requestType, int zipcode, int number,List<EmergencyVehicle> vehicleObj) throws IOException {
+            String newContent="";
             for(EmergencyVehicle e : vehicleObj){
                 if(e.vehicleType == requestType && e.zipCode==zipcode){
                     if( e.availability==1){
-                        e.decreaseAvailability(number);
-                        System.out.println("The vehicle dispatched : "+e.vehicleID);
+                        //e.decreaseAvailability(number);
+
+                        System.out.println("The vehicle dispatched : "+e.vehicleID+"\n\n");
+                        e.availability=0;
                         //e.scheduleRelease(number);
                         System.out.println(e);
                         break;
                     }
                 }
-               /* else{
-                    //algorithm part to be added
+                /*else{
+                    System.out.println("Using algorithm to find the nearest vehicle and make its availability 0");
                 }*/
+                newContent = e.vehicleID+","+e.zipCode+","+e.vehicleType+","+e.availability+"\n"+newContent;
 
             }
+            File myFoo = new File("data/EmergencyVehicle.txt");
+            FileWriter fooWriter = new FileWriter(myFoo, false); // true to append
+            // false to overwrite.
+            fooWriter.write(newContent);
+            fooWriter.close();
 
         }
 
-    /*List<RequestComplete> requestComplete = new ArrayList<>();
-    File completeFile = new File("data/RequestComplete");
-    BufferedReader bufferedReader = new BufferedReader(new FileReader(completeFile));
-    String line1="";
-            while ((line1 = bufferedReader.readLine()) != null) {
-        String[] lines = line1.split(",");
-        RequestVehicle requestCompleted = completeRequest(lines);
-        requestComplete.add(requestCompleted);
-    }
-            bufferedReader.close();*/
 
         private static EmergencyVehicle createEmergencyVehicle(String[] attributes) {
             int vehicleID = Integer.parseInt(attributes[0]);
@@ -93,8 +69,35 @@ import java.util.List;
 
              return new RequestVehicle(vehicleType, zipCode, number);
         }
-        public static void startRequest(){
+        public static void startRequest() throws IOException{
+            List<EmergencyVehicle> vehicleObjList = new ArrayList<>();
+            File distanceFile = new File("data/EmergencyVehicle.txt");
+            BufferedReader br = new BufferedReader(new FileReader(distanceFile));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] attributes = line.split(",");
+                EmergencyVehicle emergencyVehicles = createEmergencyVehicle(attributes);
+                vehicleObjList.add(emergencyVehicles);
+            }
+            br.close();
 
+            List<RequestVehicle> requestObjList = new ArrayList<>();
+            File reqFile = new File("data/TableRequest.csv");
+            BufferedReader breader = new BufferedReader(new FileReader(reqFile));
+            String line1 = "";
+            while ((line1 = breader.readLine()) != null) {
+                String[] lines = line1.split(",");
+                RequestVehicle requestVehicles = createRequestVehicle(lines);
+                requestObjList.add(requestVehicles);
+            }
+            breader.close();
+
+            for (RequestVehicle r : requestObjList) {
+                System.out.println("VehicleType: " + r.vehicleType);
+                System.out.println("ZipCode: " + r.zipCode);
+                System.out.println("Requested Vehicles: " + r.number);
+                requestVehicle(r.vehicleType, r.zipCode, r.number, vehicleObjList);
+            }
         }
         public static void completeRequest(){
 
