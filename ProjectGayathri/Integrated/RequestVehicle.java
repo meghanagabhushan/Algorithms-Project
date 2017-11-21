d24f450d59ca82a76da44f8cf3cf4deca8231801
“jpbm7” <gayathri.ksr43@gmail.com>
2017-11-21 00:22:33 -0600

    Integrated

---
+++ ProjectGayathri/Integrated/RequestVehicle.java
@@ -0,0 +1,33 @@
+package Integrated;
+public class RequestVehicle {
+    int vehicleType;
+    int zipCode;
+    int number;
+
+    public RequestVehicle(int vehicleType, int zipCode, int number) {
+
+        this.vehicleType = vehicleType;
+        this.zipCode = zipCode;
+        this.number = number;
+    }
+
+    public int getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(int vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+    @Override
+    public String toString() {
+        return "Vehicle Info: [vehicleType=" + vehicleType + ", zipcode=" + zipCode +" ]";
+    }
+}
